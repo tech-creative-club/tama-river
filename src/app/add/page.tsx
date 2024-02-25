f6d1d6150590e379014416cad0a6981bbc283b97
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-25 12:32:53 +0900

    未ログイン時飛ばす

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,6 +1,12 @@
 import { Notification } from "@/components/Notification";
-
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 export default function AddArticles() {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+  if (status === "unauthenticated") {
+    router.push("/");
+  }
   return (
     <div className="flex flex-col items-center min-h-screen py-2">
       <div className="w-full max-w-7xl p-5 pb-0">

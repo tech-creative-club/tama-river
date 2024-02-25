38c7c79e39097c58186754f3f39430c6be30cbef
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-25 09:48:39 +0900

    API作成

--- src/app/api/events/delete/route.ts
+++ src/app/api/events/delete/route.ts
@@ -1,9 +1,25 @@
-async function Handler() {
-  // const content: SummaryCard[] = randomItems();
-  const content: SummaryCardProp[] = await getArchiveByFQDN(
-    "www.city.chofu.lg.jp",
+import { SummaryCardProp } from "@/components/SummaryCard";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function Handler(req :NextApiRequest, res: NextApiResponse) {
+  const defaultData = await (await fetch("/api/events")).json() as SummaryCardProp[];
+  const filterdData = defaultData.filter((e) => e.url !== req.body.url);
+  const response = await fetch(
+    "https://tama-river-workers.suguru-toyohara.workers.dev/api/events/post",
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": process.env.POST_TOKEN ?? "",
+      },
+      body: JSON.stringify({
+        FQDN: "www.city.chofu.lg.jp",
+        data: filterdData,
+      }),
+    },
   );
-  return Response.json(content);
 }
 
 export { Handler as POST };

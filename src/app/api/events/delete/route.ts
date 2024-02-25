94ff0eebcf7923080d1a2e33f53dbc18b2990d19
suguru-toyohara <suguru_toyohara@aceof.technology>
2024-02-25 10:40:33 +0900

    API作成

--- src/app/api/events/delete/route.ts
+++ src/app/api/events/delete/route.ts
@@ -1,9 +1,15 @@
 import { SummaryCardProp } from "@/components/SummaryCard";
-import { NextApiRequest, NextApiResponse } from "next";
 
-async function Handler(req :NextApiRequest, res: NextApiResponse) {
+export const runtime = 'edge';
+
+export interface DeleteEventPostData {
+  url: string;
+}
+async function Handler(request: Request) {
+  const req = await request.json() as DeleteEventPostData;
+  const deleteTarget = req.url;
   const defaultData = await (await fetch("/api/events")).json() as SummaryCardProp[];
-  const filterdData = defaultData.filter((e) => e.url !== req.body.url);
+  const filterdData = defaultData.filter((e) => e.url !== deleteTarget);
   const response = await fetch(
     "https://tama-river-workers.suguru-toyohara.workers.dev/api/events/post",
     {
@@ -20,6 +26,29 @@ async function Handler(req :NextApiRequest, res: NextApiResponse) {
       }),
     },
   );
+  return response;
 }
+
+// async function Handler(req : NextApiRequest & DeleteEventPostData, res: NextApiResponse) {
+//   const deleteTarget = req.url;
+//   const defaultData = await (await fetch("/api/events")).json() as SummaryCardProp[];
+//   const filterdData = defaultData.filter((e) => e.url !== deleteTarget);
+//   const response = await fetch(
+//     "https://tama-river-workers.suguru-toyohara.workers.dev/api/events/post",
+//     {
+//       method: "POST",
+//       mode: "cors",
+//       cache: "no-cache",
+//       headers: {
+//         "Content-Type": "application/json",
+//         "X-API-KEY": process.env.POST_TOKEN ?? "",
+//       },
+//       body: JSON.stringify({
+//         FQDN: "www.city.chofu.lg.jp",
+//         data: filterdData,
+//       }),
+//     },
+//   );
+// }
 
 export { Handler as POST };
